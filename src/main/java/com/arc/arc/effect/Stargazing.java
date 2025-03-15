21ccd6bbf6665ca879c4cf2b32e41b0b1f7c4192
hedukai <961285867@qq.com>
2025-03-16 05:12:07 +0800

    砍了粒子数量，法阵添加了流动效果，注册了观星与繁星闪烁buff

---
+++ src/main/java/com/arc/arc/effect/Stargazing.java
@@ -0,0 +1,10 @@
+package com.arc.arc.effect;
+
+import net.minecraft.world.effect.InstantenousMobEffect;
+import net.minecraft.world.effect.MobEffectCategory;
+
+public class Stargazing extends InstantenousMobEffect {
+    public Stargazing() {
+        super(MobEffectCategory.NEUTRAL, 0x00FF00);
+    }
+}
