94df6dd8f0b823f7e9dacaf934f1110d972f9cb7
arc <1919463065@qq.com>
2025-03-17 15:33:56 +0800

    搬ArcbladeTransformed(观星大剑),换新版无坚不摧

---
+++ src/main/java/com/arc/arc/skill/ArcbladeTransformedSkill.java
@@ -0,0 +1,21 @@
+package com.arc.arc.skill;
+
+import com.google.common.collect.Lists;
+import com.p1nero.invincible.client.keymappings.InvincibleKeyMappings;
+import com.p1nero.invincible.skill.ComboBasicAttack;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.world.item.ItemStack;
+import yesman.epicfight.client.input.EpicFightKeyMappings;
+import yesman.epicfight.world.capabilities.entitypatch.player.PlayerPatch;
+import yesman.epicfight.world.capabilities.item.CapabilityItem;
+
+import java.util.List;
+
+public class ArcbladeTransformedSkill extends ComboBasicAttack{
+    public ArcbladeTransformedSkill(Builder builder) {
+        super(builder);
+    }
+}
